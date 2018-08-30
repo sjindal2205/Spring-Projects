dad42104037f588d4f1d7d843783cdfc8a7f4db2
sjindal2205 <surabhi.jindal22@gmail.com>
2018-08-30 19:52:01 +0530

    first commit

---
+++ src/main/java/com/training/SpringBooMvcApplication.java
@@ -0,0 +1,12 @@
+package com.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBooMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBooMvcApplication.class, args);
+	}
+}
